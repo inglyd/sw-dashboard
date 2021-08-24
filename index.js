6c3f079a0a155455b726cec1a3a8af1a14aedd5c
inglyd <inglydalves@outlook.com>
2021-08-24 01:42:12 -0300

    criando tabela c JQuery

--- index.js
+++ index.js
@@ -7,19 +7,30 @@ preencherQuantidade();
 preencherTabela();
 
 function preencherQuantidade() {
-  Promise.all([swapiGet('people/'), swapiGet('vehicles/ '), swapiGet('planets/ '), swapiGet('starships/ ')]).then(
-    function (results) {
-      quantPersonagens.innerHTML = results[0].data.count;
-      quantLuas.innerHTML = results[1].data.count;
-      quantPlanetas.innerHTML = results[2].data.count;
-      quantNaves.innerHTML = results[3].data.count;
-    },
-  );
+  Promise.all([
+    swapiGet('people/'),
+    swapiGet('vehicles/ '),
+    swapiGet('planets/ '),
+    swapiGet('starships/ '),
+  ]).then(function (results) {
+    quantPersonagens.innerHTML = results[0].data.count;
+    quantLuas.innerHTML = results[1].data.count;
+    quantPlanetas.innerHTML = results[2].data.count;
+    quantNaves.innerHTML = results[3].data.count;
+  });
 }
 
 async function preencherTabela() {
   const response = await swapiGet('films/');
-  
+  const tableData = response.data.results
+  tableData.forEach( film => {
+$("#films-table").append(`<tr>
+<td>${film.title}</td>
+<td>${film.release_date}</td>
+<td>${film.director}</td>
+<td>${film.episode_id}</td>
+</tr>`)
+  })
 }
 
 function swapiGet(param) {
